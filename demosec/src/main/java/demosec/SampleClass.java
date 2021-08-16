8e767f008a9919661272ab04e7a5fadeab33a7da
shubham <shubham@DESKTOP-SD6KL11>
2021-08-16 14:09:29 +0530

    first commit

---
+++ demosec/src/main/java/demosec/SampleClass.java
@@ -0,0 +1,12 @@
+package demosec;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Hello");
+
+	}
+
+}
